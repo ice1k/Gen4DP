2199071606c65fe0e96f7b68a0a0d6e4d947b6fa
ice1000 <ice1000kotlin@foxmail.com>
2016-12-28 18:29:01 +0800

    lots of updates

---
+++ src/dp/core/check.go
@@ -0,0 +1,198 @@
+package dp
+
+import (
+	"dp/err"
+	"dp/util/algo"
+	"strings"
+)
+
+const (
+	BRACE_SMALL_RIGHT  = ')'
+	BRACE_SMALL_LEFT   = '('
+	BRACE_MIDDLE_LEFT  = '['
+	BRACE_MIDDLE_RIGHT = ']'
+	BRACE_LARGE_LEFT   = '{'
+	BRACE_LARGE_RIGHT  = '}'
+)
+
+const (
+	BRACE_SMALL  = 0x000
+	BRACE_MIDDLE = 0x001
+	BRACE_LARGE  = 0x002
+)
+
+func getBraceLeft(mark int) byte {
+	switch mark {
+	case BRACE_SMALL:
+		return BRACE_SMALL_LEFT
+	case BRACE_LARGE:
+		return BRACE_LARGE_LEFT
+	case BRACE_MIDDLE:
+		return BRACE_MIDDLE_LEFT
+	}
+	panic("Mark not found")
+}
+
+func getBraceRight(mark int) byte {
+	switch mark {
+	case BRACE_SMALL:
+		return BRACE_SMALL_RIGHT
+	case BRACE_LARGE:
+		return BRACE_LARGE_RIGHT
+	case BRACE_MIDDLE:
+		return BRACE_MIDDLE_RIGHT
+	}
+	panic("Mark not found")
+}
+
+func getMark(left byte) int {
+	switch left {
+	case BRACE_SMALL_LEFT:
+		return BRACE_SMALL
+	case BRACE_LARGE_LEFT:
+		return BRACE_LARGE
+	case BRACE_MIDDLE_LEFT:
+		return BRACE_MIDDLE
+	}
+	return -1
+}
+
+func isLeftBrace(i byte) bool {
+	return i == BRACE_LARGE_LEFT ||
+		i == BRACE_MIDDLE_LEFT ||
+		i == BRACE_SMALL_LEFT
+}
+
+func isRightBrace(i byte) bool {
+	return i == BRACE_LARGE_RIGHT ||
+		i == BRACE_MIDDLE_RIGHT ||
+		i == BRACE_SMALL_RIGHT
+}
+
+func isAlpha(b byte) bool {
+	return (b >= 'a' && b <= 'z') || (b >= 'A' && b <= 'Z')
+}
+
+func isDigit(b byte) bool {
+	return b >= '0' && b <= '9'
+}
+
+func isValid(b byte) bool {
+	return isAlpha(b) || b == '$' || b == '_'
+}
+
+func isOperator(b byte) bool {
+	return b == '+' ||
+		b == '-' ||
+		b == '/' ||
+		b == '|' ||
+		b == '*' ||
+		b == '(' ||
+		b == ')' ||
+		b == '%' ||
+		b == '&' ||
+		b == '!' ||
+		b == '~' ||
+		b == '^'
+}
+
+func checkBrace(str string) {
+	stack := algo.NewStack(100)
+	line := 1
+	row := 0
+	for i := 0; i < len(str); i++ {
+		if str[i] != '\n' {
+			row++
+		} else {
+			line++
+			row = 0
+		}
+		if isLeftBrace(str[i]) {
+			stack.Push(getMark(str[i]))
+		} else if isRightBrace(str[i]) {
+			if stack.IsEmpty() {
+				err.RaiseFormat("Unexpected '%c'.", str[i])
+			} else if getBraceRight(stack.Top()) != str[i] {
+				err.RaiseFormat(
+					"Brace doesn't match, expected '%c', found '%c' at line %d, row %d.",
+					getBraceRight(stack.Top()),
+					str[i],
+					line,
+					row)
+			}
+			stack.Pop()
+		}
+	}
+	if !stack.IsEmpty() {
+		err.Raise("Expected ending brace.")
+	}
+}
+
+func runSymbolCheck(name string) bool {
+	isNum := false
+	lastOneIsOperator := true
+	for j := 0; j < len(name); j++ {
+		if lastOneIsOperator {
+			lastOneIsOperator = false
+			if !isValid(name[j]) {
+				isNum = true
+			}
+		}
+		if isOperator(name[j]) {
+			isNum = false
+			lastOneIsOperator = true
+			continue
+		}
+		if !isDigit(name[j]) {
+			if isNum || !isAlpha(name[j]) {
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func runExprDimCheck(expr string, dim int) bool {
+	inBrace := false
+	lastBraceIndex := -1
+	for j := 0; j < len(expr); j++ {
+		if !inBrace && expr[j] == '[' {
+			inBrace = true
+			lastBraceIndex = j
+		}
+		if inBrace && expr[j] == ']' {
+			inBrace = false
+			foundDim := strings.Count(expr[lastBraceIndex:j], ",") + 1
+			if foundDim != dim {
+				err.RaiseFormat(
+					"Require %d indexes, found %d.",
+					dim,
+					foundDim)
+				return false
+			}
+		}
+	}
+	return true
+}
+
+func checkSymbol(info *dyProInfo) {
+	for _, i := range info.State.DimExpr {
+		if !runSymbolCheck(i) {
+			err.RaiseFormat("Invalid name in expression: %s.", i)
+		}
+	}
+}
+
+func checkDimension(info *dyProInfo) {
+	for i := 0; i < len(info.State.DimExpr); i++ {
+		for j := i + 1; j < len(info.State.DimExpr); j++ {
+			if info.State.DimExpr[i] == info.State.DimExpr[j] {
+				err.RaiseFormat("Dimension index name redefined: %s.",
+					info.State.DimExpr[i])
+			}
+		}
+	}
+	for _, i := range info.Branches {
+		runExprDimCheck(i.Expression, len(info.State.DimExpr))
+	}
+}
---
+++ src/dp/core/repl.go
@@ -0,0 +1,30 @@
+package dp
+
+import (
+	"dp/util/sb"
+	"fmt"
+	"strings"
+	"dp/err"
+)
+
+func StartRepl() {
+	fmt.Println("Gen4DP v1.0, open source under Apache 2.0 license.")
+	fmt.Print(">> ")
+	sb := sb.NewStringBuffer()
+	var s string
+	for true {
+		fmt.Scanln(&s)
+		fmt.Println(s)
+		if strings.Trim(s, " \n\t") != "end" {
+			sb.Append(s)
+			fmt.Print(">> ")
+		} else {
+			in := sb.ToString()
+			fmt.Println(in)
+			fmt.Println(Parse(in).GenerateClang(*NewCodeStyle()))
+			fmt.Println(err.GetErrors())
+			err.Clear()
+			break
+		}
+	}
+}
